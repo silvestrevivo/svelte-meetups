3e789310e8c9720158d2257f31a464caf92639f7
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-12 09:12:05 +0200

    implementing custom store

--- src/App.svelte
+++ src/App.svelte
@@ -4,33 +4,9 @@
   import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
   import EditMeetup from "./Meetups/EditMeetup.svelte";
+  import meetups from "./Meetups/meetups-store.js";
 
   let _editMode;
-
-  let meetups = [
-    {
-      id: "m1",
-      title: "Coding Bootcamp",
-      subtitle: "Learn code in 2 hours",
-      description: "In this bootcamp, you will learn everything about code",
-      imageUrl:
-        "http://blog.hackerrank.com/wp-content/uploads/2014/08/Bootcamp.jpg",
-      address: "27th Nerd Road, 1913LC Sevilla",
-      contactEmail: "silvestre@hotmail.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "Swim Bootcamp",
-      subtitle: "Learn to swim in 2 hours",
-      description: "In this course, you will learn to swim",
-      imageUrl:
-        "http://lahinchseaworld.com/wp-content/uploads/2013/07/swimming-lesson.jpg",
-      address: "27th Nerd Road, 1913LC Castilleja",
-      contactEmail: "swim@hotmail.com",
-      isFavorite: false
-    }
-  ];
 
   function addMeetup(event) {
     const {
@@ -41,8 +17,7 @@
       address,
       imageUrl
     } = event.detail;
-    const newMeetup = {
-      id: Math.random().toString(),
+    const meetupData = {
       title,
       subtitle,
       email,
@@ -52,18 +27,13 @@
       contactEmail: email
     };
 
-    meetups = [newMeetup, ...meetups];
+    meetups.addMeetup(meetupData);
     _editMode = null;
   }
 
   function toggleFavorite(event) {
     const id = event.detail;
-    const updatedMeetup = { ...meetups.find(m => m.id === id) };
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const meetupIndex = meetups.findIndex(m => m.id === id);
-    const updatedMeetups = [...meetups];
-    updatedMeetups[meetupIndex] = updatedMeetup;
-    meetups = updatedMeetups;
+    meetups.toogleFavorite(id);
   }
 
   function cancelEdit() {
@@ -88,5 +58,5 @@
   {#if _editMode === 'Add'}
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
-  <MeetupGrid {meetups} on:toggleFavorite={toggleFavorite} />
+  <MeetupGrid meetups={$meetups} on:toggleFavorite={toggleFavorite} />
 </main>
---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,51 @@
+import { writable } from 'svelte/store';
+
+const meetups = writable([
+  {
+    id: "m1",
+    title: "Coding Bootcamp",
+    subtitle: "Learn code in 2 hours",
+    description: "In this bootcamp, you will learn everything about code",
+    imageUrl:
+      "http://blog.hackerrank.com/wp-content/uploads/2014/08/Bootcamp.jpg",
+    address: "27th Nerd Road, 1913LC Sevilla",
+    contactEmail: "silvestre@hotmail.com",
+    isFavorite: false
+  },
+  {
+    id: "m2",
+    title: "Swim Bootcamp",
+    subtitle: "Learn to swim in 2 hours",
+    description: "In this course, you will learn to swim",
+    imageUrl:
+      "http://lahinchseaworld.com/wp-content/uploads/2013/07/swimming-lesson.jpg",
+    address: "27th Nerd Road, 1913LC Castilleja",
+    contactEmail: "swim@hotmail.com",
+    isFavorite: false
+  }
+]);
+
+
+const customMeetupsStore = {
+  subscribe: meetups.subscribe,
+  addMeetup: (meetupData) => {
+    const newMeetup = {
+      ...meetupData,
+      id: Math.random().toString(),
+      isFavorite: false
+    };
+
+    meetups.update(items => [newMeetup, ...items]);
+  },
+  toogleFavorite: (id) => {
+    meetups.update(items => {
+      const updatedMeetup = { ...items.find(m => m.id === id) };
+      updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+      const meetupIndex = items.findIndex(m => m.id === id);
+      const updatedMeetups = [...items];
+      updatedMeetups[meetupIndex] = updatedMeetup;
+      return updatedMeetups;
+    })
+  }
+}
+export default customMeetupsStore;
